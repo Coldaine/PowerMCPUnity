e730548cfbb90c4abe27834f5f13de89eba344cb
Koji Hasegawa <hasegawa@hubsys.co.jp>
2025-06-20 01:01:16 +0900

    Fix run tests

--- UnityNaturalMCPServer/Editor/McpTools/RunTestsTool/RunTestsTool.cs
+++ UnityNaturalMCPServer/Editor/McpTools/RunTestsTool/RunTestsTool.cs
@@ -1,20 +1,21 @@
+using System;
 using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using ModelContextProtocol.Server;
 using UnityEditor.TestTools.TestRunner.Api;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace UnityNaturalMCP.Editor.McpTools.RunTestsTool
 {
     [McpServerToolType]
     public static class RunTestsTool
     {
-        [McpServerTool(Name = "RunTests"), Description(
-             "Run tests on Unity Test Runner. Specifying TestMode is required. Recommend filtering by assemblyNames, groupNames, or testNames to narrow down the tests to be executed to the scope of changes.")]
-        public static async ValueTask<string> RunTests(
-            [Description("An enum flag that specifies if EditMode or PlayMode tests should run.")]
-            TestMode testMode,
+        [McpServerTool, Description(
+             "Run Edit Mode tests on Unity Test Runner. Recommend filtering by assemblyNames, groupNames, or testNames to narrow down the tests to be executed to the scope of changes.")]
+        public static async ValueTask<string> RunEditModeTests(
             [Description(
                 "The name of assemblies included in the run. That is the assembly name, without the .dll file extension. E.g., MyTestAssembly")]
             string[] assemblyNames = null,
@@ -29,19 +30,59 @@ namespace UnityNaturalMCP.Editor.McpTools.RunTestsTool
             string[] testNames = null,
             CancellationToken cancellationToken = default)
         {
+            return await RunTests(TestMode.EditMode, assemblyNames, categoryNames, groupNames, testNames,
+                cancellationToken);
+        }
+
+        [McpServerTool, Description(
+             "Run Play Mode tests on Unity Test Runner. Recommend filtering by assemblyNames, groupNames, or testNames to narrow down the tests to be executed to the scope of changes.")]
+        public static async ValueTask<string> RunPlayModeTests(
+            [Description(
+                "The name of assemblies included in the run. That is the assembly name, without the .dll file extension. E.g., MyTestAssembly")]
+            string[] assemblyNames = null,
+            [Description(
+                "The name of a Category to include in the run. Any test or fixtures runs that have a Category matching the string.")]
+            string[] categoryNames = null,
+            [Description(
+                "The same as testNames, except that it allows for Regex. This is useful for running specific fixtures or namespaces. E.g. \"^MyNamespace\\.\" Runs any tests where the top namespace is MyNamespace.")]
+            string[] groupNames = null,
+            [Description(
+                "The full name of the tests to match the filter. This is usually in the format FixtureName.TestName. If the test has test arguments, then include them in parenthesis. E.g. MyTestClass2.MyTestWithMultipleValues(1).")]
+            string[] testNames = null,
+            CancellationToken cancellationToken = default)
+        {
+            return await RunTests(TestMode.PlayMode, assemblyNames, categoryNames, groupNames, testNames,
+                cancellationToken);
+        }
+
+        private static async ValueTask<string> RunTests(
+            TestMode testMode,
+            string[] assemblyNames = null,
+            string[] categoryNames = null,
+            string[] groupNames = null,
+            string[] testNames = null,
+            CancellationToken cancellationToken = default)
+        {
             var filter = new Filter
             {
                 assemblyNames = assemblyNames, categoryNames = categoryNames,
                 groupNames = groupNames, testNames = testNames, testMode = testMode,
             };
-            var testResultCollector = new TestResultCollector();
-            TestRunnerApi.RegisterTestCallback(testResultCollector);
+            Debug.Log($"Running tests, {filter}");
 
-            var testRunner = ScriptableObject.CreateInstance<TestRunnerApi>();
-            var guid = testRunner.Execute(new ExecutionSettings(filter));
+            TestResultCollector testResultCollector = null;
+            TestRunnerApi testRunner = null;
 
             try
             {
+                await UniTask.SwitchToMainThread();
+
+                testResultCollector = new TestResultCollector();
+                TestRunnerApi.RegisterTestCallback(testResultCollector);
+
+                testRunner = ScriptableObject.CreateInstance<TestRunnerApi>();
+                var guid = testRunner.Execute(new ExecutionSettings(filter));
+
                 var result = await testResultCollector.WaitForRunFinished(cancellationToken);
                 if (!cancellationToken.IsCancellationRequested)
                 {
@@ -51,10 +92,17 @@ namespace UnityNaturalMCP.Editor.McpTools.RunTestsTool
                 TestRunnerApi.CancelTestRun(guid);
                 return "Test run cancelled.";
             }
+            catch (Exception e)
+            {
+                Debug.LogError($"Error running tests: {e.Message}");
+                return $"Error running tests: {e.Message}";
+            }
             finally
             {
-                TestRunnerApi.UnregisterTestCallback(testResultCollector);
-                Object.Destroy(testRunner);
+                if (testResultCollector != null)
+                    TestRunnerApi.UnregisterTestCallback(testResultCollector);
+                if (testRunner != null)
+                    Object.Destroy(testRunner);
             }
         }
     }
